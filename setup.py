a20e385900d2176861a5cd2846a012cc14653755
neil.howell <neil.howell@accruent.com>
2021-04-12 07:10:21 -0400

    update accruent rf parser to new parser that allows skips

--- setup.py
+++ setup.py
@@ -1,30 +1,36 @@
 from setuptools import find_packages, setup
+import version
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    print(LONG_DESCRIPTION)
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
 
 setup(
-      name='robotframework-historic-parser',
-      version="0.1.4",
-      description='Parser to push robotframework execution results to MySQL',
-      classifiers=[
+    name='robotframework-historic-parser',
+    version=version.VERSION,
+    description='Parser to push robotframework execution results to MySQL',
+    classifiers=[
           'Framework :: Robot Framework',
           'Programming Language :: Python',
           'Topic :: Software Development :: Testing',
-      ],
-      keywords='robotframework historical execution report parser',
-      author='Shiva Prasad Adirala',
-      author_email='adiralashiva8@gmail.com',
-      url='https://github.com/adiralashiva8/robotframework-historic-parser',
-      license='MIT',
+    ],
+    keywords='robotframework historical execution report parser',
+    author='Shiva Prasad Adirala',
+    author_email='adiralashiva8@gmail.com',
+    url='https://github.com/adiralashiva8/robotframework-historic-parser',
+    license='MIT',
 
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
 
-      install_requires=[
-          'mysql-connector',
-      ],
-      entry_points={
-          'console_scripts': [
-              'rfhistoricparser=robotframework_historic_parser.runner:main',
-          ]
-      },
+    install_requires=REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+          'rfhistoricparser=robotframework_historic_parser.parserargs:main',
+        ]
+    },
 )
