dd4767f920c0bb811c3c2a3c3cbe06c74a6c4647
neil.howell <neil.howell@accruent.com>
2021-11-05 13:57:22 -0400

    begin on function_test.py

--- robotframework_historic_parser/rfhistoricparser.py
+++ robotframework_historic_parser/rfhistoricparser.py
@@ -171,8 +171,7 @@ def get_time_in_min(time_str):
     """Method converting time to minutes"""
     h, m, s = time_str.split(':')
     ctime = int(h) * 3600 + int(m) * 60 + int(s)
-    crtime = float("{0:.2f}".format(ctime / 60))
-    return crtime
+    return float("{0:.2f}".format(ctime / 60))
 
 
 def connect_to_mysql_db(host, port, user, pwd, db):
